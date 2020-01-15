a01b74cace4eaf09d577ca94b70625cec3cd9fd8
Anca-Oprea <opreaanca1987@gmail.com>
2020-01-15 12:31:25 +0100

    Updates

---
+++ precache-manifest.24f8c5c899b099622a3f897b8b7e0406.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecead67ea5933226518529d649f63ea9",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "10bcea99493b50000e0c",
+    "url": "/shopping-cart/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a6523dfe53ce7aabea1d",
+    "url": "/shopping-cart/static/css/main.a335a9c9.chunk.css"
+  },
+  {
+    "revision": "10bcea99493b50000e0c",
+    "url": "/shopping-cart/static/js/2.4ba6f6ba.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/shopping-cart/static/js/2.4ba6f6ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6523dfe53ce7aabea1d",
+    "url": "/shopping-cart/static/js/main.88723927.chunk.js"
+  },
+  {
+    "revision": "13437d0b965bfbfef91f",
+    "url": "/shopping-cart/static/js/runtime-main.276815e1.js"
+  },
+  {
+    "revision": "e244085ab7e1571fe0db7a2bda80c69b",
+    "url": "/shopping-cart/static/media/logo.e244085a.svg"
+  }
+]);
